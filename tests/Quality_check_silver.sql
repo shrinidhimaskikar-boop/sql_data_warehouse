f912dcef0a2da1c4c52fed990dde1c2ce22d105d
shrinidhimaskikar-boop <maskikarsrinidhi@gmail.com>
2026-08-20 19:45:48 +0530

    Enhance comments in Quality_check_silver.sql
    Added detailed comments explaining the purpose and checks performed by the Quality Check script for the silver layer.

---
+++ tests/Quality_check_silver.sql
@@ -0,0 +1,21 @@
+/*==========================================================================================================
+Quality Checks Silver Layer:
+=============================================================================================================
+
+Purpose:
+The purpose of this script is to check the quality , consistency , formating , standarlization  and accuracy of 
+data present in the silver layer.It includes checking of 
+
+-Checking for duplicate or NULL  values for primary keys.
+-Unwanted spacing present in the string format vaues.
+-Data standarlization and consitency.
+-Checking for invalid date ranges.
+
+*/
+
+-- ==============================================================================================================
+--CHECKING: 'silver.crm_cust_id'
+-- =============================================================================================================
+--1.Checking for NULL or duplicate primary key for customer 
+
+
