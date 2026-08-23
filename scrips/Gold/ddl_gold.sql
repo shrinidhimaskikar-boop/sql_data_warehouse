3a2cbeda144b89d5398ec9d99d7cbc13c1b498b2
shrinidhimaskikar-boop <maskikarsrinidhi@gmail.com>
2026-08-23 11:58:47 +0530

    Create view gold.dim_customers for customer details
    Create a view for customer data with various attributes.

--- scrips/Gold/ddl_gold.sql
+++ scrips/Gold/ddl_gold.sql
@@ -1,1 +1,38 @@
 
+
+/*============================================================================================================
+  Creating dimension: gold.dim_customers
+  Execution : SELECT * FROM gold.dim_customers
+==============================================================================================================*/
+
+
+IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
+    DROP VIEW gold.dim_customers;
+GO
+
+CREATE VIEW gold.dim_customers AS
+SELECT
+    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Creating  surrogate key
+    ci.cst_id  AS Customer_id,
+    ci.cst_key  AS Customer_number,
+    ci.cst_firstname   AS First_name,
+    ci.cst_lastname AS Last_name,
+    la.cntry AS Country,
+    ci.cst_marital_status AS Marital_status,
+    CASE 
+        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr  -- Here crm is the primary source
+        ELSE COALESCE(ca.gen, 'n/a')  			         --If condition not satisfied them it will consider values from erp source
+    END AS Gender,
+    ca.bdate AS Birthdate,
+    ci.cst_create_date AS Create_date
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+    ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+    ON ci.cst_key = la.cid;
+GO
+
+/*============================================================================================================
+  Creating dimension: gold.dim_products
+  Execution : SELECT * FROM gold.dim_products
+==============================================================================================================*/
